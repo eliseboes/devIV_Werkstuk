9d13c9d1d36e25df68b246022d4d1ba3da33a692
Elise Boes <elise.boes@student.ehb.be>
2020-04-28 17:11:28 +0200

    Display genre tags

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
  'Use strict'
 
  async function init() {
-     const entryList = new Entrylist();
-     await entryList.fetch();
-     entryList.getGenres();
-    // entryList.sortGenres();
-    // entryList.showGenres();
+    const entryList = new Entrylist();
+    await entryList.fetch();
+    entryList.getGenres();
+    entryList.sortGenres();
+    entryList.showGenres();
     entryList.showEntries();
  }
 
@@ -31,7 +31,6 @@
          } catch (err) {
              console.log(err)
              throw new Error(`could not fetch data ${err}`);
-             // showError(err.message);
          } finally {}
      }
      getGenres() {
@@ -39,14 +38,13 @@
          this.entries.forEach(item => {
             if(item.genre != undefined){
                 //Tolowercase en trim gebruikt om de genres die verkeerd geschreven zijn toch te kunnen 'reducen'
-                this.genres.push(new Genre(item['genre'].toLowerCase().trim()))
+                this.genres.push(item['genre'].toLowerCase().trim())
             }
-            console.log(item)
          });
      }
      sortGenres() {
          //reduce de genres om de labels en aantallen te verkrijgen
-         this.genreNames = this.genreNames.reduce(groupBy, {})
+         this.genres = this.genres.reduce(groupBy, {})
          function groupBy(acc, genre) {
              const count = acc[genre] || 0;
              return {
@@ -54,17 +52,17 @@
                  [genre]: count + 1
              }
          }
+         console.log(this.genres)
      }
      showGenres() {
-         this.genreNames.forEach(genre => 
-            this.genreTagElement.insertAdjacentHTML(`beforeend`, `<li class='${genre.code}'>${genre.name}</li>`)
+         Object.keys(this.genres).forEach(genre => 
+            this.genreTagElement.insertAdjacentHTML(`beforeend`, `<li class='${genre}'>${genre}</li>`)
         )
      }
      showEntries(){
          this.entries.forEach(entry =>{
-             console.log(entry)
              let videoImg = entry.thumbnail.url;
-              this.resultElement.insertAdjacentHTML('beforeend', `<figure>
+              this.resultElement.insertAdjacentHTML('beforeend', `<figure class=${entry.genre}>
               <img src=${videoImg}>
                 <figcaption>
                     <h3>${entry.name}</3>
@@ -82,7 +80,7 @@
          this.code = entry['genre-v2'];
      }
      get htmlString() {
-         return `<li class="${this.code}${this.name}</li>`
+         return `<li class="${this.name}">${this.name}</li>`
      }
  }
 
