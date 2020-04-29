9750a0553d0bf8ec2a4dc86440d2470f7f8c3607
Elise Boes <elise.boes@student.ehb.be>
2020-04-29 22:04:37 +0200

    Filteren genres (correctie)

--- js/main.js
+++ js/main.js
@@ -1,82 +1,99 @@
-// 'Use strict'
-let entries;
-let selectedDoelgroep;
-var selectedGenres = ["comedy", "dans", "theater", "muziektheater", "multidisciplinair", "circus", "opera", "familie", "concert", "figurentheater"];
+//'Use strict'
 
-$(function(){
- 
-//Roep de gewenste functies aan bij het laden van de pagina
-fetchData();
+    let entries;
+    let selectedDoelgroep;
+    //let selectedGenres = ["comedy", "dans", "theater", "muziektheater", "multidisciplinair", "circus", "opera", "concert", "figurentheater"];
+    let selectedGenres = []
 
-async function fetchData() {
-    try {
-        //Gebruik de async await syntax om de data met 'fetch' op te halen via de URL en om de JSON data hier uit te halen
-        //Gebruik de try catch syntax om eventuele errors af te handelen
-        const response = await fetch('/data/entries.json')
-        const data = await response.json()
-        entries = data.items;
-        getGenres();
-    } catch (err) {
-        console.log(err)
-        throw new Error(`could not fetch data ${err}`);
-    } finally {}
-}
+    //Roep de gewenste functies aan bij het laden van de pagina
+    fetchData();
 
-let addEventListeners = () => {
-    let tags = document.querySelectorAll('.tag');
-    let entries = document.querySelectorAll('.entry');
-    selectedGenres = [];
-    tags.forEach(evalTag => {
-        evalTag.addEventListener('click', function (e) {
-            if (this.classList.contains('selected')) {
-                this.classList.remove('selected');
-                $(`.entry.${this.classList[0]}`).show();
-            } else {
-                $(`.entry.${this.classList[0]}`).hide();
-                this.classList.add('selected');
-            }
+    async function fetchData() {
+        try {
+            //Gebruik de async await syntax om de data met 'fetch' op te halen via de URL en om de JSON data hier uit te halen
+            //Gebruik de try catch syntax om eventuele errors af te handelen
+            const response = await fetch('/data/entries.json')
+            const data = await response.json()
+            entries = data.items;
+            getGenres();
+        } catch (err) {
+            console.log(err)
+            throw new Error(`could not fetch data ${err}`);
+        } finally {}
+    }
+
+    let updateTags = (state, classList) => {
+        if (state == 'on') {
+            selectedGenres.push(classList);
+        } else {
+            selectedGenres.pop(classList)
+        }
+        console.log(selectedGenres)
+    }
+
+    let showTags = sortedGenres => {
+        const resultElement = document.getElementById('results');
+        const genreTagElement = document.getElementById('genretags');
+        const doelgroepenTagElement = document.getElementById('doelgroepentags');
+        //Loop over de 'key' en 'value' pairs van de gesorteerde lijst
+        Object.entries(sortedGenres).forEach(genre =>
+            genreTagElement.insertAdjacentHTML('beforeend', `<li class='${genre[0]} tag'>
+            ${genre[0]}
+                <span class="amount">
+                    ${genre[1]}
+                </span>
+            </li>`)
+        )
+        doelgroepenTagElement.insertAdjacentHTML('beforeend', `
+        <li class='volwassenen tag'>
+           volwassenen
+        </li>
+        <li class='familie tag'>
+           familie
+        </li>
+        `)
+    }
+
+    let showResults = () => {
+        $('#results').empty();
+        let resultElement = document.getElementById('results');
+        entries.forEach(entry => {
+            selectedGenres.forEach(genre => {
+                if (entry.genre != undefined && entry.genre.toLowerCase().trim() == genre) {
+                    let videoImg = entry.thumbnail.url;
+                    resultElement.insertAdjacentHTML('beforeend', `<figure class=${entry.genre}>
+            <img src=${videoImg}>
+              <figcaption>
+                  <h3>${entry.name}</3>
+                  <p>${entry.excerpt}</p>
+                  <p>${entry['video-length']}</p>
+              </figcaption>
+            </figure>`)
+                }
+            });
         })
-    })
-};
 
-let showElements = sortedGenres => {
-    const resultElement = document.getElementById('results');
-    const genreTagElement = document.getElementById('genretags');
-    const doelgroepenTagElement = document.getElementById('doelgroepentags');
-    //Loop over de 'key' en 'value' pairs van de gesorteerde lijst
-    Object.entries(sortedGenres).forEach(genre =>
-        genreTagElement.insertAdjacentHTML('beforeend', `<li class='${genre[0]} tag'>
-        ${genre[0]}
-            <span class="amount">
-                ${genre[1]}
-            </span>
-        </li>`)
-    )
-    doelgroepenTagElement.insertAdjacentHTML('beforeend', `
-    <li class='volwassenen tag'>
-       volwassenen
-    </li>
-    <li class='familie tag'>
-       familie
-    </li>
-    `)
-    entries.forEach(entry => {
-        selectedGenres.forEach(genre => {
-            if (entry.genre != undefined && entry.genre.toLowerCase().trim() == genre) {
-                let videoImg = entry.thumbnail.url;
-                let genreClass = entry.genre.toLowerCase().trim();
-                resultElement.insertAdjacentHTML('beforeend', `<figure class='${genreClass} entry'>
-        <img src='${videoImg}'>
-          <figcaption>
-              <h3>${entry.name}</3>
-              <p>${entry.excerpt}</p>
-              <p>${entry['video-length']}</p>
-          </figcaption>
-        </figure>`)
+        let updateTags = (state, classList) => {
+            if (state == 'on') {
+                selectedGenres.push(classList);
+            } else {
+                selectedGenres.splice(selectedGenres.indexOf(classList), 1)
             }
-        });
+            console.log(selectedGenres)
+        }
+
+    $('.tag').click(function () {
+        if (this.classList.contains('selected')) {
+            console.log(this.classList)
+            this.classList.remove('selected');
+            updateTags('off', this.classList[0]);
+        } else {
+            this.classList.add('selected');
+            updateTags('on', this.classList[0])
+        }
+        showResults();
+        showTags();
     })
-    addEventListeners();
 }
 
 let getGenres = () => {
@@ -103,7 +120,8 @@ sortGenres = genres => {
             [genre]: count + 1
         }
     }
-    showElements(sortedGenres);
+    showTags(sortedGenres);
+    showResults();
 }
 
 // function filterDoelgroepen() {
@@ -122,5 +140,4 @@ sortGenres = genres => {
 // //     )
 // //     entryList.entries = getTheater(entryList.entries);
 // //     console.log(entryList.entries)
-// // }   
-})
+// // }
