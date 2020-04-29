29407b9183639ce63ab1203ccc6e74d7e4cfae1c
Elise Boes <elise.boes@student.ehb.be>
2020-04-29 22:17:48 +0200

    Fixed genre filtering (non-functional)

--- js/main.js
+++ js/main.js
@@ -1,50 +1,43 @@
 //'Use strict'
+let entries;
+let selectedDoelgroep;
+//let selectedGenres = ["comedy", "dans", "theater", "muziektheater", "multidisciplinair", "circus", "opera", "concert", "figurentheater"];
+let selectedGenres = []
 
-    let entries;
-    let selectedDoelgroep;
-    //let selectedGenres = ["comedy", "dans", "theater", "muziektheater", "multidisciplinair", "circus", "opera", "concert", "figurentheater"];
-    let selectedGenres = []
+//Roep de gewenste functies aan bij het laden van de pagina
+fetchData();
 
-    //Roep de gewenste functies aan bij het laden van de pagina
-    fetchData();
+async function fetchData() {
+    try {
+        //Gebruik de async await syntax om de data met 'fetch' op te halen via de URL en om de JSON data hier uit te halen
+        //Gebruik de try catch syntax om eventuele errors af te handelen
+        const response = await fetch('/data/entries.json')
+        const data = await response.json()
+        //Sla entries op in globale variabele
+        entries = data.items;
+        getGenres();
+    } catch (err) {
+        console.log(err)
+        throw new Error(`could not fetch data ${err}`);
+    } finally {}
+}
 
-    async function fetchData() {
-        try {
-            //Gebruik de async await syntax om de data met 'fetch' op te halen via de URL en om de JSON data hier uit te halen
-            //Gebruik de try catch syntax om eventuele errors af te handelen
-            const response = await fetch('/data/entries.json')
-            const data = await response.json()
-            entries = data.items;
-            getGenres();
-        } catch (err) {
-            console.log(err)
-            throw new Error(`could not fetch data ${err}`);
-        } finally {}
-    }
-
-    let updateTags = (state, classList) => {
-        if (state == 'on') {
-            selectedGenres.push(classList);
-        } else {
-            selectedGenres.pop(classList)
-        }
-        console.log(selectedGenres)
-    }
-
-    let showTags = sortedGenres => {
-        const resultElement = document.getElementById('results');
-        const genreTagElement = document.getElementById('genretags');
-        const doelgroepenTagElement = document.getElementById('doelgroepentags');
-        //Loop over de 'key' en 'value' pairs van de gesorteerde lijst
-        Object.entries(sortedGenres).forEach(genre =>
-            genreTagElement.insertAdjacentHTML('beforeend', `<li class='${genre[0]} tag'>
+let showTags = sortedGenres => {
+    //Haal HTML-elementen op
+    const resultElement = document.getElementById('results');
+    const genreTagElement = document.getElementById('genretags');
+    const doelgroepenTagElement = document.getElementById('doelgroepentags');
+    //Loop over de 'key' en 'value' pairs van de gesorteerde lijst
+    //Insert HTML elementen met properties uit de entry objecten
+    Object.entries(sortedGenres).forEach(genre =>
+        genreTagElement.insertAdjacentHTML('beforeend', `<li class='${genre[0]} tag'>
             ${genre[0]}
                 <span class="amount">
                     ${genre[1]}
                 </span>
             </li>`)
-        )
-        doelgroepenTagElement.insertAdjacentHTML('beforeend', `
+    )
+    doelgroepenTagElement.insertAdjacentHTML('beforeend', `
         <li class='volwassenen tag'>
            volwassenen
         </li>
@@ -52,16 +45,16 @@
            familie
         </li>
         `)
-    }
+}
 
-    let showResults = () => {
-        $('#results').empty();
-        let resultElement = document.getElementById('results');
-        entries.forEach(entry => {
-            selectedGenres.forEach(genre => {
-                if (entry.genre != undefined && entry.genre.toLowerCase().trim() == genre) {
-                    let videoImg = entry.thumbnail.url;
-                    resultElement.insertAdjacentHTML('beforeend', `<figure class=${entry.genre}>
+let showResults = () => {
+    $('#results').empty();
+    let resultElement = document.getElementById('results');
+    entries.forEach(entry => {
+        selectedGenres.forEach(genre => {
+            if (entry.genre != undefined && entry.genre.toLowerCase().trim() == genre) {
+                let videoImg = entry.thumbnail.url;
+                resultElement.insertAdjacentHTML('beforeend', `<figure class=${entry.genre}>
             <img src=${videoImg}>
               <figcaption>
                   <h3>${entry.name}</3>
@@ -69,28 +62,31 @@
                   <p>${entry['video-length']}</p>
               </figcaption>
             </figure>`)
-                }
-            });
-        })
-
-        let updateTags = (state, classList) => {
-            if (state == 'on') {
-                selectedGenres.push(classList);
-            } else {
-                selectedGenres.splice(selectedGenres.indexOf(classList), 1)
             }
-            console.log(selectedGenres)
+        });
+    })
+
+    let updateTags = (state, classList) => {
+        if (state == 'on') {
+            selectedGenres.push(classList);
+        } else {
+            selectedGenres.splice(selectedGenres.indexOf(classList), 1)
         }
+        console.log(selectedGenres)
+    }
 
     $('.tag').click(function () {
+        //Check of de tag al geselecteerd is
         if (this.classList.contains('selected')) {
-            console.log(this.classList)
+            //Is geselecteerd, de 'selected' class wordt verwijderd en de array van geselecteerd tags wordt geupdatet
             this.classList.remove('selected');
             updateTags('off', this.classList[0]);
         } else {
+            //Is niet geselecteerd, de 'selected' class wordt toegevoegd en de array van geselecteerd tags wordt geupdatet
             this.classList.add('selected');
             updateTags('on', this.classList[0])
         }
+        //Update de resulten en tags wanneer een tag geselecteerd of gedeselecteerd wordt
         showResults();
         showTags();
     })
